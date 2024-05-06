39937362972cb92730759bd36249a263122cb9e3
Volodymyr Kotsiubenko <kotsubenkovova@gmail.com>
2024-05-06 16:55:40 +0300

    Initial commit

---
+++ LocaLoc/LocaLocApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaLocApp.swift
+//  LocaLoc
+//
+//  Created by Volodymyr Kotsiubenko on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LocaLocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
