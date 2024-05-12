7ab221be66a6a806648e4b0593f96cef8f3bd177
Volodymyr Kotsiubenko <kotsubenkovova@gmail.com>
2024-05-12 21:38:17 +0300

    CI test

--- LocaLocUITests/LocaLocUITests.swift
+++ LocaLocUITests/LocaLocUITests.swift
@@ -31,17 +31,17 @@ final class LocaLocUITests: XCTestCase {
         app.launch()
     }
     
-    func testExample3() throws {
-        let app = XCUIApplication()
-        app.launch()
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testExample3() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//    }
+//
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
