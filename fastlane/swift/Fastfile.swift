a85427835c1c94576e7bea84c70d10e3eea64742
Volodymyr Kotsiubenko <kotsubenkovova@gmail.com>
2024-05-12 12:21:37 +0300

    Test lane

--- fastlane/swift/Fastfile.swift
+++ fastlane/swift/Fastfile.swift
@@ -9,6 +9,12 @@ open class Fastfile: LaneFile {
     override public init() {
         super.init()
     }
+    
+    func testLane() {
+        desc("Test lane description")
+        /// Makes a build and runs a tests
+        scan()
+    }
 }
 
 // Please don't remove the lines below
